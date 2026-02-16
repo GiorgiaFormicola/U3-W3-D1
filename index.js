ae51039237ac7c28a32aa54a6967742f97b0f412
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-16 14:31:45 +0100

    Exercise 2 done

--- index.js
+++ index.js
@@ -6,3 +6,10 @@
 - undefined
 - null
 - any*/
+// EXERCISE 2
+var name = "Giorgia";
+console.log("ES.2_NOME", name);
+var age = 28;
+console.log("ES.2_ETA'", age);
+var studyingTS = true;
+console.log("ES.2_STA STUDIANDO TYPESCRIPT?", studyingTS);
--- index.ts
+++ index.ts
@@ -6,3 +6,13 @@
 - undefined
 - null
 - any*/
+
+// EXERCISE 2
+let name: string = "Giorgia";
+console.log("ES.2_NOME", name);
+
+let age: number = 28;
+console.log("ES.2_ETA'", age);
+
+let studyingTS: boolean = true;
+console.log("ES.2_STA STUDIANDO TYPESCRIPT?", studyingTS);
